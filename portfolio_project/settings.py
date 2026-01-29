c30142ab20684ae8b58f974e3eb9bbafccf76210
Rodolpho Pinheiro <rhbpinheiro30@gmail.com>
2026-01-29 14:14:13 -0300

    Configure WhiteNoise for static files

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Redirecionamento após login (vai para o dashboard)
 LOGIN_REDIRECT_URL = 'dashboard'
